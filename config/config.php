2cb27d275298f2eb895158e01d03f53978d79f01
M. Yassine Bouchama <149491865+MYassineBouch@users.noreply.github.com>
2024-05-24 11:13:14 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,1 +1,6 @@
+<?php
+define('HOST', ''); # serveur où sera stocké la base des données
+define('DB', ''); # nom de la base
+define('USER', ''); # utilisateur (admin)
+define('PASSWORD', ''); # mot de passe
 
