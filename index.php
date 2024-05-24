3640845c4e5082d62e9bcdc398924c2195776d0d
M. Yassine Bouchama <149491865+MYassineBouch@users.noreply.github.com>
2024-05-24 11:18:10 +0200

    Update index.php
    index.php crée et initialisé

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
+<?php
+error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+ini_set('display_errors', 1);
+    //The front root controller
+
+    //The requested route
+    $route = null;
+    if (isset($_GET['route'])) {
+        $route = $_GET['route'];
+    }
+
+    //We switch to the good controller
+    switch ($route) {
+        case null:
+            require('views/welcome_view.php');
+            break;
+      # les controleurs :
+        default:
+            require('views/404_view.php');
+            break;
+
+
+    }
 
