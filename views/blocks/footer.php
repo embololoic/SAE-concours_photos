beb81ab3a21975f65e94c53fca6ceb0863de6fe0
M. Yassine Bouchama <149491865+MYassineBouch@users.noreply.github.com>
2024-05-24 11:30:48 +0200

    Update footer.php

--- views/blocks/footer.php
+++ views/blocks/footer.php
@@ -1,6 +1,6 @@
 <footer>
 	<span>Groupe TP2 - BUT1 Réseaux & Télécommunication</span>
-	<img src="./images/rt.png" alt="R&T" />
+	<img src="./layout/images/rt.png" alt="R&T" />
 </footer>
 
 </body>
