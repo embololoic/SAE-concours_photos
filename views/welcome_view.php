dde976570fe4d336f570af6287c93e079ddbd5e8
M. Yassine Bouchama <149491865+MYassineBouch@users.noreply.github.com>
2024-05-24 11:23:57 +0200

    Update welcome_view.php

--- views/welcome_view.php
+++ views/welcome_view.php
@@ -1,1 +1,9 @@
+<?php require('header.php')?>
+
+<h2>Page d'accueil</h2>
+<p><em>En cours de développement...</em></p>
+<p>Pour le moment la seule possibilité est de s'assurer que le site fonctionne..<p>
+
+<?php
+require('footer.php');
 
