7ff826c96a46585acb4e1006a950464c17f81bd2
mbouch46 <v3l9s005@gmail.com>
2024-05-27 09:28:14 +0200

    test jules

--- views/welcome_view.php
+++ views/welcome_view.php
@@ -1,6 +1,6 @@
 <?php require('header.php')?>
 
-<h2>Page d'accueil</h2>
+<h2>Page d'accuel</h2>
 <p><em>En cours de développement...</em></p>
 <p>Pour le moment la seule possibilité est de s'assurer que le site fonctionne..<p>
 
