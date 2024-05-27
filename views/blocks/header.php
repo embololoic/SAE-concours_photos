a752927e2997397fdeb7a032fa0b031a5632e203
mbouch46 <v3l9s005@gmail.com>
2024-05-27 10:25:51 +0200

    - header.php (css fixes) (ajout font-awesome)

--- views/blocks/header.php
+++ views/blocks/header.php
@@ -11,13 +11,13 @@
 <header>
 	<nav>
         <ul>
-            <li><a href="./index.php?req=welcome_view"><img src="./layout/images/idea.png" alt="Retour vers la page d'accueil" /></a></li>
+            <li><a href="./index.php?req=welcome_view"><i class="fa fa-lightbulb"></i></a></li>
             <li><a href="index.php?route=participer">Participer</a></li>
             <li><a href="index.php?route=voter">Voter</a></li>
             <li><a href="index.php?route=resultats">Résultats</a></li>
             <div class="search-bar">
                 <input type="text" placeholder="Rechercher des photos...">
-                <button type="submit"><i class="fa fa-search"></i></button>
+                <button type="submit"></button>
             </div>
         </ul>
     </nav>
