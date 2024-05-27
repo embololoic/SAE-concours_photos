91af407dfed1cbd79710ad743592f0b704420754
mbouch46 <v3l9s005@gmail.com>
2024-05-27 10:17:55 +0200

    - header.php (css fixes)

--- views/blocks/header.php
+++ views/blocks/header.php
@@ -2,16 +2,15 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Appli procédures</title>
-    <link rel="stylesheet" type="text/css" href="css/global.css" />
+    <title>Concours Photo</title>
+    <link rel="stylesheet" type="text/css" href="./layout/global.css" />
 </head>
 
 <body>
 <header>
-	<a href="./index.php?req=welcome_view"><img src="images/idea.png" alt="Retour vers la page d'accueil" /></a>
-	<h1>Councours photo</h1>
 	<nav>
         <ul>
+            <li><a href="./index.php?req=welcome_view">Accueil</a></li> 
             <li><a href="index.php?route=participer">Participer</a></li>
             <li><a href="index.php?route=voter">Voter</a></li>
             <li><a href="index.php?route=resultats">Résultats</a></li>
