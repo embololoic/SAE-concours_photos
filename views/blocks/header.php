601e77214edb574ef031f06b06d41b47a13b5f4f
mbouch46 <v3l9s005@gmail.com>
2024-05-27 10:17:27 +0200

    - header.php (fixes)

--- views/blocks/header.php
+++ views/blocks/header.php
@@ -8,13 +8,17 @@
 
 <body>
 <header>
-	<a href="index.php"><img src="images/idea.png" alt="Retour vers la page d'accueil" /></a>
+	<a href="./index.php?req=welcome_view"><img src="images/idea.png" alt="Retour vers la page d'accueil" /></a>
 	<h1>Councours photo</h1>
 	<nav>
         <ul>
             <li><a href="index.php?route=participer">Participer</a></li>
             <li><a href="index.php?route=voter">Voter</a></li>
             <li><a href="index.php?route=resultats">Résultats</a></li>
+            <div class="search-bar">
+                <input type="text" placeholder="Rechercher des photos...">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </div>
         </ul>
     </nav>
 </header>
