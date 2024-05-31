cdad93d546cbab5a661819d3770eecce479c58e4
EMBOLO FOUDA LOIC <148533384+embololoic@users.noreply.github.com>
2024-05-31 11:39:31 +0200

    Update login_model.php
    version1.0.0.

--- models/login_model.php
+++ models/login_model.php
@@ -1,1 +1,59 @@
 
+<?php
+session_start();
+
+// Configuration for the LDAP server
+define('SERVER', 'ldaps://ldapsupannappli.univ-poitiers.fr:636');
+define('ROOT', 'ou=people,dc=univ-poitiers,dc=fr');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $login = $_POST['login'];
+    $pass = $_POST['pass'];
+
+    $connex = ldap_connect(SERVER);
+    ldap_set_option($connex, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+    if ($connex) {
+        ldap_bind($connex);
+        $req = 'supannAliasLogin=' . $login;
+        $res = ldap_search($connex, ROOT, $req);
+        $entries = ldap_get_entries($connex, $res);
+        
+        if ($entries['count'] > 0) {
+            $uid = $entries[0]['uid'][0];
+            $dn = 'uid=' . $uid . ',' . ROOT;
+
+            if (ldap_bind($connex, $dn, $pass)) {
+                $_SESSION['user_id'] = $uid;
+                header('Location: vote.php');
+                exit();
+            } else {
+                echo "<p>Incorrect login or password.</p>";
+            }
+        } else {
+            echo "<p>User not found.</p>";
+        }
+        ldap_close($connex);
+    } else {
+        echo "Unable to connect to LDAP server.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <form action="login.php" method="post">
+        <label for="login">Login:</label>
+        <input type="text" id="login" name="login" required>
+        <br>
+        <label for="pass">Password:</label>
+        <input type="password" id="pass" name="pass" required>
+        <br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
