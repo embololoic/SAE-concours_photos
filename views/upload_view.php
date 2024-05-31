1a615eeeb2a5028121f026c540ca6af03faaec3d
mbouch46 <v3l9s005@gmail.com>
2024-05-31 10:33:12 +0200

    fixes upload_view.php

--- views/upload_view.php
+++ views/upload_view.php
@@ -1,8 +1,8 @@
 <?php require('./views/blocks/header.php')?>
 
 <body>
-<h1>Uploader une photo</h1>
 <div class="upload-form">
+    <h1>Uploader une photo</h1>
     <form action="./index.php?=upload" method="post" enctype="multipart/form-data">
         <label for="photo">Sélectionner votre photo :</label>
         <input type="file" id="photo" name="photo" accept="image/*" required>
@@ -10,7 +10,7 @@
         <label for="titre">Légende* :</label>
         <input type="text" id="legende" name="legende" required>
         <br><br>
-        <input type="submit" value="uploader">
+        <input type="submit" value="Participer!">
     </form>
 </div>
 </body>
