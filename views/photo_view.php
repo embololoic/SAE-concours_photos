6d4a8061ffa5017fddbbd98d052750244401c6ab
mbouch46 <v3l9s005@gmail.com>
2024-05-31 11:00:06 +0200

    fixes  : - CSS - Voting view

--- views/photo_view.php
+++ views/photo_view.php
@@ -16,14 +16,46 @@
   <caption>TEMPS RESTANT ...</caption>
   <table>
   <tr>
-    <td style="border:1px solid black"></td>
-    <td style="border:1px solid black"></td>
-    <td style="border:1px solid black"></td>
-    <td style="border:1px solid black"></td>
-    <td style="border:1px solid black"></td>
-    <td style="border:1px solid black"></td>
+      <td id="days"></td>
+      <td>:</td>
+      <td id="hours"></td>
+      <td>:</td>
+      <td id="minutes"></td>
+      <td>:</td>
+      <td id="seconds"></td>
   </tr>
   </table>
+    <script>
+        // la date limite
+        const targetDate = new Date('2024-06-05T00:00:00');
+
+        function updateCountdown() {
+            const currentDate = new Date();
+            const timeDifference = targetDate.getTime() - currentDate.getTime();
+
+            if (timeDifference <= 0) {
+                // Countdown has ended
+                document.getElementById('days').textContent = '0';
+                document.getElementById('hours').textContent = '0';
+                document.getElementById('minutes').textContent = '0';
+                document.getElementById('seconds').textContent = '0';
+                return;
+            }
+
+            const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+            document.getElementById('days').textContent = days;
+            document.getElementById('hours').textContent = hours.toString().padStart(2, '0');
+            document.getElementById('minutes').textContent = minutes.toString().padStart(2, '0');
+            document.getElementById('seconds').textContent = seconds.toString().padStart(2, '0');
+        }
+
+        // Update the countdown every second
+        setInterval(updateCountdown, 1000);
+    </script>
 </fieldset>
 </div>
 <?php
