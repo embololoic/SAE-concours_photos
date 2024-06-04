2cbcf3004e3d3055760f651b75a8e0bac8c38634
jules <messinjules@gmail.com>
2024-06-04 11:39:14 +0200

    vffuffyu

--- control/ctrl_login.php
+++ control/ctrl_login.php
@@ -4,3 +4,13 @@ function user_login()
 {
         require('./models/login_model.php');
 }
+
+function check_login()
+{
+        session_start();
+        if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
+              header("Location: ./index.php");
+              exit();
+        }
+
+}
--- index.php
+++ index.php
@@ -17,25 +17,35 @@ ini_set('display_errors', 1);
             break;
 
         case 'welcome':
+            require('control/ctrl_login.php');
+            check_login();
             require('views/welcome_view.php');
             break;
 
         case 'upload':
+            require('control/ctrl_login.php');
+            check_login();
             require('control/ctrl_photo.php');
-            upload_photo_controller();
+
             break;
 
         case 'photos';
+            require('control/ctrl_login.php');
+            check_login();
             require('control/ctrl_photo.php');
             list_photo();
             break;
 
         case 'voter':
+            require('control/ctrl_login.php');
+            check_login();
             require('control/ctrl_photo.php');
             list_photo();
             break;
 
         case 'resultats':
+            require('control/ctrl_login.php');
+            check_login();
             require('control/ctrl_vote.php');
             fetch_votes();
             break;
